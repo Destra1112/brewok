3543c1e485a981727d997d848ba9c3f96094fc17
AngkasaBoy <80139186+AngkasaBoy@users.noreply.github.com>
2021-07-04 14:32:57 +0800

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>• Owner : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n• Tutorial Clone : <a href='https://t.me/LEOANGKASAAA'>Click here</a>\n○ Channel : <a href='https://t.me/rebellionnss'>Tekan Disini</a>\n• Group : <a href='https://t.me/storyangkasa'>Tekan Disini</a>\n• Owner Ganteng: @LEOANGKASAAA</b>",
+            text = f"<b>• Owner : <a href='tg://user?id={OWNER_ID}'>OWNER CAKEP</a>\n○ BAHASA : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n• Tutorial Clone : <a href='https://t.me/LEOANGKASAAA'>TEKAN DISINI</a>\n• CHANNEL ASUPAN : <a href='https://t.me/desahannsange'>ASUPAN DISINI</a>\n• SUPPORT CHANNEL : <a href='https://t.me/storyangkasa'>SUPPORT CHANNEL</a>\n• OWNER REPO: @LEOANGKASAAA</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
