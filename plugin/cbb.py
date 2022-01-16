f67923c4d72a0b6cd5d90eed93f80330a135c52f
Destra1112 <97810837+Destra1112@users.noreply.github.com>
2022-01-16 11:36:16 +0700

    Update and rename plugins/cbb.py to plugin/cbb.py

--- plugin/cbb.py
+++ plugin/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>• Owner : <a href='tg://user?id={OWNER_ID}'>OWNER CAKEP</a>\n○ BAHASA : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n• Tutorial Clone : <a href='https://t.me/LEOANGKASAAA'>TEKAN DISINI</a>\n• CHANNEL ASUPAN : <a href='https://t.me/desahannsange'>ASUPAN DISINI</a>\n• SUPPORT CHANNEL : <a href='https://t.me/storyangkasa'>SUPPORT CHANNEL</a>\n• OWNER REPO: @LEOANGKASAAA</b>",
+            text = f"<b>• Owner : <a href='tg://user?id={OWNER_ID}'>OWNER CAKEP</a>\n○ BAHASA : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n• Tutorial Clone : <a href='https://t.me/DESSXTRASH'>TEKAN DISINI</a>\n• CHANNEL ASUPAN : <a href='https://t.me/sobatanime'>ASUPAN DISINI</a>\n• SUPPORT CHANNEL : <a href='https://t.me/Duniaanimehot'>SUPPORT CHANNEL</a>\n• OWNER REPO: @DessXtrash</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
